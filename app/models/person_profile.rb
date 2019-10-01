47b4b57a27938b3dc6cacd06db97df34b9f52075
prydunmv <prydunmv@gmail.com>
2019-10-01 15:55:13 +0300

    fixed the bugs

--- app/models/person_profile.rb
+++ app/models/person_profile.rb
@@ -1,3 +1,8 @@
 class PersonProfile < ApplicationRecord
   belongs_to :account
+
+ def show
+ def index
+   	@profile = Profile.find(params[:id])
+@personal_profile = current_account.personal_profile
 end
