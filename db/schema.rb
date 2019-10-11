38896217515185caeff7a777c04c108c936402c2
prydunmv <prydunmv@gmail.com>
2019-10-11 21:55:04 +0300

    Added roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_30_174749) do
+ActiveRecord::Schema.define(version: 2019_10_11_183100) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2019_09_30_174749) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "accounts_roles", force: :cascade do |t|
+    t.integer "account_id"
+    t.integer "role_id"
   end
 
   create_table "ideas", force: :cascade do |t|
@@ -60,6 +65,12 @@ ActiveRecord::Schema.define(version: 2019_09_30_174749) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["account_id"], name: "index_person_profiles_on_account_id"
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "votes", force: :cascade do |t|
