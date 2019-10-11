01568c3b3d915137711992c7311ba675e21dda9a
prydunmv <prydunmv@gmail.com>
2019-10-11 21:55:04 +0300

    Added roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    # stored_location_for(resource) || root_path
+    if resource.roles.find_by(name: 'admin')
+      admin_root_path
+    else
+      root_path
+    end
+  end
 end
