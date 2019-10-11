1dc4b97981edf85973fc7aa658ba2da0f1f9c66a
prydunmv <prydunmv@gmail.com>
2019-10-11 21:55:16 +0300

    Added active admin

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,20 @@ ActiveRecord::Schema.define(version: 2019_10_11_183100) do
     t.integer "role_id"
   end
 
+  create_table "active_admin_comments", force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_type"
+    t.integer "resource_id"
+    t.string "author_type"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.text "description"
