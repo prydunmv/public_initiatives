c7f8c9c0db01a5c5ce222bd4588453ce5567039b
prydunmv <prydunmv@gmail.com>
2019-10-22 20:45:00 +0300

    #21 show initiatives

--- app/controllers/initiatives_controller.rb
+++ app/controllers/initiatives_controller.rb
@@ -1,5 +1,10 @@
 class InitiativesController < ApplicationController
-
+  def index
+    @initiatives = Initiative.all
+  end
+  def show
+    @initiative = Initiative.find(params[:id])
+  end
   def destroy
   	@initiative = Initiative.find(params[:id])
 	@initiative.destroy
